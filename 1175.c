2e8e6a59bced5a27fb76e099720f1744ec9f19fe
Leonardo Alencar <108036203+leogalencar@users.noreply.github.com>
2023-07-06 10:28:40 -0300

    Update 1175.c

--- 1175.c
+++ 1175.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int array[20], reversed_array[20];
 
+    for (int i = 0, j = 19; i < 20; i++, j--)
+    {
+        scanf("%d", &array[i]);
+        reversed_array[j] = array[i];
+    }
+
+    for (int i = 0; i < 20; i++)
+    {
+        printf("N[%d] = %d\n", i, reversed_array[i]);
+    }
+ 
+    return 0;
+}
